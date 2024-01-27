c5164e0e045719c14efa6d4c83b5088891ac818c
Criss <cgutierrez.infor@gmail.com>
2024-01-26 22:07:40 -0300

    fix: timestamp  delete

--- sql_func.py
+++ sql_func.py
@@ -89,7 +89,7 @@ def crear_xml(row):
     referencia = ET.SubElement(documento, "Referencia")
     ET.SubElement(referencia, "NroLinRef").text = str((int(((row['NroLinRef'])))))
     #ET.SubElement(referencia, "TpoDocRef").text = str((int(((row['TpoDocRef'])))))
-    ET.SubElement(referencia, "TpoDocRef").text = row['FolioRef'].split('-')[0] if '-' in row['FolioRef'] else row['FolioRef']
+    ET.SubElement(referencia, "TpoDocRef").text = str(row['FolioRef']).split('-')[0] if '-' in str(row['FolioRef']) else str(row['FolioRef'])
     ET.SubElement(referencia, "FolioRef").text = str(row['FolioRef'])
     ET.SubElement(referencia, "FchRef").text = str(row['FchRef'])
     ET.SubElement(referencia, "CodRef").text = str(row['CodRef'])
