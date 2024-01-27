0f376ff680135bc7b6405c2a683e081a4adc9ec0
Criss <cgutierrez.infor@gmail.com>
2024-01-26 22:04:08 -0300

    fix: timestamp  delete

--- sql_func.py
+++ sql_func.py
@@ -88,7 +88,8 @@ def crear_xml(row):
     # Sección Referencia
     referencia = ET.SubElement(documento, "Referencia")
     ET.SubElement(referencia, "NroLinRef").text = str((int(((row['NroLinRef'])))))
-    ET.SubElement(referencia, "TpoDocRef").text = str((int(((row['TpoDocRef'])))))
+    #ET.SubElement(referencia, "TpoDocRef").text = str((int(((row['TpoDocRef'])))))
+    ET.SubElement(referencia, "TpoDocRef").text = str(int(row['TpoDocRef']) if pd.notna(row['TpoDocRef']) and str(row['TpoDocRef']).isdigit() else 0)
     ET.SubElement(referencia, "FolioRef").text = str(row['FolioRef'])
     ET.SubElement(referencia, "FchRef").text = str(row['FchRef'])
     ET.SubElement(referencia, "CodRef").text = str(row['CodRef'])
